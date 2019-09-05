b07389726cc88bc40c94f3d4e4e1ee1546d8d626
Thomas Krieger <thomas.krieger@puppet.com>
2019-09-05 11:18:13 +0100

    check system requirements

--- plans/install.pp
+++ plans/install.pp
@@ -3,10 +3,11 @@ plan remediate_install::install (
   String[1] $install_docker,
   String[1] $remove_old,
   String[1] $init_swarm,
-  String $license_file = undef,
-  String $compose_version = '1.24.1',
-  String $win_install_dir = 'c:\remediate',
-  String $unix_install_dir = '/opt/remediate'
+  String $license_file     = undef,
+  String $compose_version  = '1.24.1',
+  String $win_install_dir  = 'c:\remediate',
+  String $unix_install_dir = '/opt/remediate',
+  Boolean $enforce_system_requirements = false
 ) {
 
   if(($install_docker != 'n') and ($install_docker != 'y')) {
@@ -28,6 +29,80 @@ plan remediate_install::install (
   run_plan(facts, nodes => $nodes)
   $myfacts = get_targets($nodes)[0].facts
 
+  # check system requirements
+
+  # check hardware platform
+  if($myfacts['os']['hardware'] != 'x86_64') {
+    if($enforce_system_requirements) {
+      fail_plan("Remediate is not supported on ${myfacts['os']['name']} hardware")
+    } else {
+      out::message("Remediate is not supported on ${myfacts['os']['name']} hardware")
+    }
+
+  }
+
+  # check os version and
+  case $myfacts['os']['name'] {
+    'RedHat', 'CentOS':  {
+      if($myfacts['os']['release']['major'] < '7') {
+        if($enforce_system_requirements) {
+          fail_plan("Remediate is not supported on Redhat/CentOS version ${myfacts['os']['releae']['major']}. It has to be at least 7.")
+        } else {
+          out::message("Remediate is not supported on Redhat/CentOS version ${myfacts['os']['releae']['major']}. It has to be at least 7.")
+        }
+      }
+    }
+    'Debian':            {
+      if($myfacts['os']['release']['major'] < '8') {
+        if($enforce_system_requirements) {
+          fail_plan("Remediate is not supported on Debian version ${myfacts['os']['releae']['major']}. It has to be at least 8.")
+        } else {
+          out::message("Remediate is not supported on Debian version ${myfacts['os']['releae']['major']}. It has to be at least 8.")
+        }
+      }
+    }
+    'Ubuntu':            {
+      if($myfacts['os']['release']['major'] < '14.04') {
+        if($enforce_system_requirements) {
+          fail_plan("Remediate is not supported on Ubuntu version ${myfacts['os']['releae']['major']}. It has to be at least 14.04.")
+        } else {
+          out::message("Remediate is not supported on Ubuntu version ${myfacts['os']['releae']['major']}. It has to be at least 14.04.")
+        }
+      }
+    }
+    'Windows':           {
+      if($myfacts['os']['release']['major'] == '10') {
+        if($enforce_system_requirements) {
+          fail_plan("Remediate is not supported on Windowa version ${myfacts['os']['releae']['major']}. It has to be at least 10.")
+        } else {
+          out::message("Remediate is not supported on Windowa version ${myfacts['os']['releae']['major']}. It has to be at least 10.")
+        }
+      }
+    }
+    default:             {
+      fail_plan("OS ${myfacts['os']['name']} is not supported.")
+    }
+  }
+
+  # check system meory
+  if($myfacts['memory']['system']['total_bytes'] < 8589934592) {
+    if($enforce_system_requirements) {
+    fail_plan('System memory has to be not lower than 8 GB.')
+    } else {
+      out::message('System memory has to be not lower than 8 GB.')
+    }
+  }
+
+  # check cpu count
+  if($myfacts['processors']['count'] < 2) {
+    if($enforce_system_requirements) {
+    fail_plan('Remediate need 2 cpus at minimum.')
+    } else {
+      out::message('Remediate need 2 cpus at minimum.')
+    }
+  }
+
+  # run installation
   if($install_docker == 'y') {
 
     # install docker
