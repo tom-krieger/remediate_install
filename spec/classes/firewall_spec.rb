23f7a4f442b61302faf7445a8cf2ce71cc9ea494
Thomas Krieger <thomas.krieger@puppet.com>
2019-09-11 10:33:28 +0200

    updated unit tests

--- spec/classes/firewall_spec.rb
+++ spec/classes/firewall_spec.rb
@@ -4,6 +4,16 @@ describe 'remediate_install::firewall' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
+      let(:facts) do
+        os_facts.merge(
+          'operatingsystemversion' => 'windows',
+          'os' => {
+            'windows' => {
+              'system32' => 'c:/windows/system32',
+            },
+          },
+        )
+      end
 
       if os =~ %r{windows}
         let(:params) do
