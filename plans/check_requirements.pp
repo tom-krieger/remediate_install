4cadd7d939040a37299dad97d47d543717420f62
Thomas Krieger <thomas.krieger@puppet.com>
2019-09-12 15:06:21 +0200

    use out::message instwad of crit

--- plans/check_requirements.pp
+++ plans/check_requirements.pp
@@ -19,51 +19,51 @@ plan remediate_install::check_requirements (
     $target.apply_prep()
     $myfacts = facts($target)
 
-    out::message('System requirements check started ...')
+    out::message("System requirements check started on ${target} ...")
 
     # check system requirements
     # check hardware platform
     if($myfacts['os']['hardware'] != 'x86_64') {
-      crit("Remediate is not supported on ${myfacts['os']['name']} hardware")
+      out::message("   => Remediate is not supported on ${myfacts['os']['name']} hardware")
     }
 
     # check os version and
     case $myfacts['os']['name'] {
       'RedHat', 'CentOS':  {
         if($myfacts['os']['release']['major'] < '7') {
-          crit("Remediate is not supported on Redhat/CentOS version ${myfacts['os']['releae']['major']}. It has to be at least 7.")
+          out::message("   => Remediate is not supported on Redhat/CentOS version ${myfacts['os']['releae']['major']}. It has to be at least 7.")
         }
       }
       'Debian':            {
         if($myfacts['os']['release']['major'] < '8') {
-          crit("Remediate is not supported on Debian version ${myfacts['os']['releae']['major']}. It has to be at least 8.")
+          out::message("   => Remediate is not supported on Debian version ${myfacts['os']['releae']['major']}. It has to be at least 8.")
         }
       }
       'Ubuntu':            {
         if($myfacts['os']['release']['major'] < '14.04') {
-          crit("Remediate is not supported on Ubuntu version ${myfacts['os']['releae']['major']}. It has to be at least 14.04.")
+          out::message("   => Remediate is not supported on Ubuntu version ${myfacts['os']['releae']['major']}. It has to be at least 14.04.")
         }
       }
       'Windows':           {
         if($myfacts['os']['release']['major'] != '10') {
-          crit("Remediate is not supported on Windows version ${myfacts['os']['release']['major']}. It is only supported on Windows 10.")
+          out::message("   => Remediate is not supported on Windows version ${myfacts['os']['release']['major']}. It is only supported on Windows 10.")
         }
       }
       default:             {
-        crit("OS ${myfacts['os']['name']} is not supported.")
+        out::message("   => OS ${myfacts['os']['name']} is not supported.")
       }
     }
 
     # check system meory
     if($myfacts['memory']['system']['total_bytes'] < 8589934592) {
-      crit('System memory has to be at least 8 GB.')
+      out::message('   => System memory has to be at least 8 GB.')
     }
 
     # check cpu count
     if($myfacts['processors']['count'] < 2) {
-      crit('Remediate need 2 cpus at minimum.')
+      out::message('   => Remediate need 2 cpus at minimum.')
     }
 
-    out::message('System requirements check finished')
+    out::message("System requirements check finished on ${target}")
   }
 }
